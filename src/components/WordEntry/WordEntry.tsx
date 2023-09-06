dad8fcf1e9dad99395d3a396fe1caae0fbc298ea
Hayden Buckley-Smith <hayden@abstack.co>
2023-09-06 16:42:44 +0100

    feat: WordEntry component

--- src/components/WordEntry/WordEntry.tsx
+++ src/components/WordEntry/WordEntry.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import { StyledWordEntry } from './WordEntry.styles';
 
-interface IWordEntryProps {}
+interface IWordEntryProps {
+  onGuessEntered: (guess: string) => void;
+}
 
-export default function WordEntry({}: IWordEntryProps) {
-  return <StyledWordEntry></StyledWordEntry>;
+export default function WordEntry({ onGuessEntered }: IWordEntryProps) {
+  const [value, setValue] = React.useState('');
+
+  const getValidWordleString = (rawString: string) => {
+    const upperCaseString = rawString.toUpperCase();
+    const validWordleString = rawString.replace(/[^a-z]/gi, '');
+
+    return validWordleString?.toUpperCase();
+  };
+
+  const handleLetterEntry = (e: any) => {
+    const validString: string = getValidWordleString(e.target.value);
+    onGuessEntered(validString);
+    setValue(validString);
+  };
+  return (
+    <StyledWordEntry
+      autoFocus
+      placeholder='Enter your guess...'
+      value={value}
+      maxLength={5}
+      onChange={(e) => handleLetterEntry(e)}
+    />
+  );
 }
