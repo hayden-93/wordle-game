81e0641caa981812afa6754985c806a4688c6af3
Hayden Buckley-Smith <hayden@abstack.co>
2023-09-06 15:50:23 +0100

    feat: WordEntry component

---
+++ src/components/WordEntry/WordEntry.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledWordEntry } from './WordEntry.styles';
+
+interface IWordEntryProps {}
+
+export default function WordEntry({}: IWordEntryProps) {
+  return <StyledWordEntry></StyledWordEntry>;
+}
