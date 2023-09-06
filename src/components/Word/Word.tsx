194d082429b9e743c70c926ae070ebb78d688e70
Hayden Buckley-Smith <hayden@abstack.co>
2023-09-06 15:20:48 +0100

    feat: Word component
    create state for guessing the word value and added a useEffect so when guessWordValue changes, it triggers and sets the state of guessValue

--- src/components/Word/Word.tsx
+++ src/components/Word/Word.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
-export interface IWordProps {}
+export interface IWordProps {
+  isWordEvaluated: boolean;
+  guessWordValue: string;
+}
 
-export default function Word({}: IWordProps) {
+export default function Word({ isWordEvaluated, guessWordValue }: IWordProps) {
+  const [guessValue, setGuessValue] = React.useState<string>('');
+
+  React.useEffect(() => {
+    setGuessValue(guessWordValue);
+  }, [guessWordValue]);
+
   return (
     <>
       <></>
