dc9972c6b3284d2f753d1454a0a17a9a831dad4b
Hayden Buckley-Smith <hayden@abstack.co>
2023-09-08 09:15:45 +0100

    feat: Wordle game functionality
    Implement Wordle game functionality, including word scoring, accuracy calculation, and answer retrieval. Initialise the word list for the game. Word evaluation tests for word-guessing game functionality

--- src/components/WordEntry/WordEntry.tsx
+++ src/components/WordEntry/WordEntry.tsx
@@ -20,6 +20,7 @@ export default function WordEntry({ onGuessEntered }: IWordEntryProps) {
     onGuessEntered(validString);
     setValue(validString);
   };
+
   return (
     <StyledWordEntry
       autoFocus
