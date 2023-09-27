31da0be2decd7921229fa5993ebd1e12dc0595bd
Hayden Buckley-Smith <hayden@abstack.co>
2023-09-27 14:01:25 +0100

    feat: answerRetriever

---
+++ src/utils/answerRetriever.ts
@@ -0,0 +1,11 @@
+import { getwordlist } from './wordleList';
+
+export let answer = '';
+
+export const retrieveAnswer = (): string => {
+  if (answer.length > 0) return answer;
+
+  const wordlelist = getwordlist();
+  answer = wordlelist[Math.floor(Math.random() * wordlelist.length)];
+  return answer;
+};
