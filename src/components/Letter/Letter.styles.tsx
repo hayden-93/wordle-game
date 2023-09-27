a481524195d4aa7721f23afaf2e2f0310c9c3a39
Hayden Buckley-Smith <hayden@abstack.co>
2023-09-27 14:38:12 +0100

    refactor: Letter component and moved accuracy into its own module

--- src/components/Letter/Letter.styles.tsx
+++ src/components/Letter/Letter.styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { AccuracyEnum, accuracyColorMap } from '../../utils/accuracy';
 
 export const StyledLetterButton = styled.button<{ accuracy: AccuracyEnum }>`
   margin: 2px;
@@ -11,17 +12,14 @@ export const StyledLetterButton = styled.button<{ accuracy: AccuracyEnum }>`
   font-weight: bold;
   background: ${(props) => accuracyColorMap.get(props.accuracy)};
 `;
-
-export enum AccuracyEnum {
-  correct = 'correct',
-  wrongPosition = 'wrongPosition',
-  None = 'None',
-  doesNotExist = 'doesNotExist',
-}
-
-export const accuracyColorMap = new Map<AccuracyEnum, string>([
-  [AccuracyEnum.correct, '#6CA965'],
-  [AccuracyEnum.wrongPosition, '#C8B653'],
-  [AccuracyEnum.None, 'black'],
-  [AccuracyEnum.doesNotExist, '#787C7F'],
-]);
+export const StyledBlankButton = styled.button`
+  margin: 2px;
+  width: 60px;
+  height: 60px;
+  border-radius: 2px;
+  color: black;
+  font-size: 30px;
+  font-weight: bold;
+  border: 2px solid #787c7f;
+  background: black;
+`;
--- src/components/Letter/Letter.tsx
+++ src/components/Letter/Letter.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import { AccuracyEnum, StyledLetterButton } from './Letter.styles';
+import { useEffect, useState } from 'react';
+import { AccuracyEnum } from '../../utils/accuracy';
+import { StyledBlankButton, StyledLetterButton } from './Letter.styles';
 
-interface ILetterProps {
-  position?: number;
+export interface ILetterProps {
+  position: number;
   value: string;
   accuracy: AccuracyEnum;
 }
 
-export default function Letter({ position, value, accuracy }: ILetterProps) {
-  return <StyledLetterButton accuracy={accuracy}>{value}</StyledLetterButton>;
-}
+export const Letter = ({ position, value, accuracy }: ILetterProps) => {
+  const [letterPosition, setLetterPosition] = useState(0);
+  const [letterValue, setLetterValue] = useState('');
+  const [letterAccuracy, setLetterAccuracy] = useState(AccuracyEnum.none);
+
+  useEffect(() => {
+    setLetterValue(value);
+  }, [value]);
+
+  useEffect(() => {
+    setLetterPosition(position);
+  }, [position]);
+
+  useEffect(() => {
+    setLetterAccuracy(accuracy);
+  }, [accuracy]);
+
+  return letterValue === '_' ? (
+    <StyledBlankButton>'_'</StyledBlankButton>
+  ) : (
+    <StyledLetterButton accuracy={letterAccuracy}>
+      {letterValue}
+    </StyledLetterButton>
+  );
+};
+
+export default Letter;
