022fdf1c0d3524a1501bec61af05e8b9ecca0f0e
Hayden Buckley-Smith <hayden@abstack.co>
2023-09-28 11:25:00 +0100

    feat: Worlde game

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,94 @@
-import React from 'react';
-import Word from './components/Word/Word';
-import WordEntry from './components/WordEntry/WordEntry';
+import React, { useEffect, useState } from 'react';
+
+import { WordBoard } from './components/WordBoard/WordBoard';
+import { WordEntry } from './components/WordEntry/WordEntry';
+import { VirtualKeyboard } from './components/VirtualKeyboard/VirtualKeyboard';
+import { StyledGameOverDisplay } from './components/WordEntry/WordEntry.styles';
+
+import { IGuess } from './utils/guess.model';
+import { retrieveAnswer } from './utils/answerRetriever';
+import './index.css';
 
 function App() {
-  const [wordGuess, setWordGuess] = React.useState('');
+  const [wordGuess, setWordGuess] = useState('');
+  const [wordGuesses, setWordGuesses] = useState<IGuess[]>([]);
+  const [nextGuessPosition, setNextGuessPosition] = useState(0);
+  const [winning, setWinning] = useState<boolean | null>(null);
+  const [gameOver, setGameOver] = useState(false);
+  const [gameOverText, setGameOverText] = useState('');
+
+  const handleGuessCompletion = (guess: string): void => {
+    if (wordGuess === retrieveAnswer().toUpperCase()) {
+      setWinning(true);
+      return;
+    }
+
+    setNextGuessPosition(nextGuessPosition + 1);
+  };
+
+  const handleWordGuesses = (guesses: IGuess[]) => {
+    setWordGuesses(guesses);
+  };
+
+  const handleOnClickedKey = (key: string): void => {
+    if (key.toLowerCase() === 'backspace') {
+      if (wordGuess.length !== 0) {
+        setWordGuess(wordGuess.substring(0, wordGuess.length - 1));
+      }
+    } else if (key.toLowerCase() === 'enter' && wordGuess.length === 5) {
+      handleGuessCompletion(wordGuess);
+    } else {
+      if (wordGuess.length < 5) {
+        setWordGuess(wordGuess + key);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (winning != null) {
+      setNextGuessPosition(0);
+      setGameOver(true);
+    }
+
+    if (winning) {
+      setGameOverText('You Won!!');
+    } else if (winning === false) {
+      setGameOverText(`Word: ${retrieveAnswer().toUpperCase()}`);
+    }
+  }, [winning]);
+
+  useEffect(() => {
+    if (nextGuessPosition === 6) {
+      setWinning(false);
+      return;
+    }
+
+    if (gameOver === true) return;
+
+    setWordGuess('');
+  }, [nextGuessPosition, gameOver]);
+
   return (
-    <div>
-      <WordEntry
-        onGuessComplete={() => setWordGuess('')}
-        onGuessEntered={(guess) => setWordGuess(guess)}
+    <div className='App-board'>
+      {gameOver ? (
+        <StyledGameOverDisplay>{gameOverText}</StyledGameOverDisplay>
+      ) : (
+        <WordEntry
+          onGuessEntered={(guess) => setWordGuess(guess)}
+          onGuessComplete={() => handleGuessCompletion(wordGuess)}
+        />
+      )}
+      <WordBoard
+        guess={wordGuess}
+        currentPosition={nextGuessPosition}
+        wordGuessesCallback={handleWordGuesses}
       />
-      <Word isWordEvaluated={false} guessWordValue={wordGuess} />
+      <VirtualKeyboard
+        onClickedKey={handleOnClickedKey}
+        wordGuesses={wordGuesses}
+      />
     </div>
   );
 }
+
 export default App;
